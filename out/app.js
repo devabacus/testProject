9d3f07beadecee602030a3a43140ae7618993e10
mrfrolk <ekaterita20@gmail.com>
2025-03-20 11:38:35 +0500

    1

--- out/app.js
+++ out/app.js
@@ -1,8 +1,7 @@
-var msg = 'Hello';
+var msg = 'привет как у тебя дела?';
 console.log(msg);
-console.log(msg);
-console.log(msg);
-console.log(msg);
-console.log(msg);
-console.log(msg);
-console.log(msg);
+var newVar = "ivan";
+function greetings(name) {
+    return "hello ".concat(name);
+}
+console.log(greetings("stas"));
--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,12 @@
-let msg = 'Hello';
+let msg = 'привет как у тебя дела?';
 console.log(msg);
+
+const newVar = "ivan";
+
+function greetings(name: string): string {
+    return `hello ${name}`;
+}
+
+
+console.log(greetings("stas"));
+
